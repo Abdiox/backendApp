f7fc5dae0519fc6afeb0d8df7e1acbc59a460556
Abdiox <abdirahmanroble2610@gmail.com>
2024-06-20 16:27:05 +0200

    så der er data klar

--- src/main/java/dat3/project/InitData.java
+++ src/main/java/dat3/project/InitData.java
@@ -41,7 +41,29 @@ public class InitData implements CommandLineRunner {
         Participant participant3 = new Participant("Mbappe", "Mand", 25, "PSG AtletikKlub");
         participantRepository.save(participant3);
 
-        Disciplin disciplin1 = new Disciplin("100-meterløb", LØB_100_METER, ResultType.TIME, participant1);
+       /* Disciplin disciplin1 = new Disciplin("100-meterløb", LØB_100_METER, ResultType.TIME, participant1);
         disciplinRepository.save(disciplin1);
+
+        */
+
+
+        List<Disciplin> discipliner = Arrays.asList(
+                new Disciplin("100-meterløb", DisciplineType.LØB_100_METER, ResultType.TIME, null),
+                new Disciplin("200-meterløb", DisciplineType.LØB_200_METER, ResultType.TIME, null),
+                new Disciplin("400-meterløb", DisciplineType.LØB_400_METER, ResultType.TIME, null),
+                new Disciplin("800-meterløb", DisciplineType.LØB_800_METER, ResultType.TIME, null),
+                new Disciplin("1500-meterløb", DisciplineType.LØB_1500_METER, ResultType.TIME, null),
+                new Disciplin("Hækkeløb", DisciplineType.HÆKKELØB, ResultType.TIME, null),
+                new Disciplin("Højdespring", DisciplineType.HØJDESPIRING, ResultType.DISTANCE, null),
+                new Disciplin("Stangspring", DisciplineType.STANGSPRING, ResultType.DISTANCE, null),
+                new Disciplin("Længdespring", DisciplineType.LÆNGDESPRING, ResultType.DISTANCE, null),
+                new Disciplin("Kuglestød", DisciplineType.KUGLESTØD, ResultType.DISTANCE, null),
+                new Disciplin("Diskoskast", DisciplineType.DISKOSKAST, ResultType.DISTANCE, null),
+                new Disciplin("Spydkast", DisciplineType.SPYDKAST, ResultType.POINTS, null)
+        );
+
+        disciplinRepository.saveAll(discipliner);
     }
-}
+
+    }
+
--- src/main/java/dat3/project/dto/DisciplinDtoResponse.java
+++ src/main/java/dat3/project/dto/DisciplinDtoResponse.java
@@ -1,5 +1,7 @@
 package dat3.project.dto;
 
+import dat3.project.Enum.DisciplineType;
+import dat3.project.Enum.ResultType;
 import dat3.project.entity.Disciplin;
 import dat3.project.entity.Result;
 import lombok.Getter;
@@ -14,13 +16,15 @@ import java.util.List;
 public class DisciplinDtoResponse {
     private int id;
     private String name;
-    private Enum resultType;
+    private DisciplineType disciplineType;
+    private ResultType resultType;
     private List<ResultDtoResponse> result;
 
 
     public DisciplinDtoResponse(Disciplin disciplin) {
         this.id = disciplin.getId();
         this.name = disciplin.getName();
+        this.disciplineType = disciplin.getDisciplineType();
         this.resultType = disciplin.getResultType();
         this.result = disciplin.getResults().stream().map(ResultDtoResponse::new).collect(java.util.stream.Collectors.toList());
     }
