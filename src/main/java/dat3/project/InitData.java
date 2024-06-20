5cd2367cd18275cda85c9e8ecdb1c19374b4d33c
Abdiox <abdirahmanroble2610@gmail.com>
2024-06-21 01:15:56 +0200

    tilføjet data til initdata

--- src/main/java/dat3/project/InitData.java
+++ src/main/java/dat3/project/InitData.java
@@ -42,13 +42,24 @@ public class InitData implements CommandLineRunner {
         Disciplin disciplin1 = new Disciplin("100-meterløb", DisciplineType.LØB_100_METER, ResultType.TIME);
         Disciplin disciplin2 = new Disciplin("200-meterløb", DisciplineType.LØB_200_METER, ResultType.TIME);
         Disciplin disciplin3 = new Disciplin("400-meterløb", DisciplineType.LØB_400_METER, ResultType.TIME);
+        Disciplin displin4 = new Disciplin("800-meterløb", DisciplineType.LØB_800_METER, ResultType.TIME);
+        Disciplin displin5 = new Disciplin("1500-meterløb", DisciplineType.LØB_1500_METER, ResultType.TIME);
+        Disciplin displin6 = new Disciplin("Hækkeløb", DisciplineType.HÆKKELØB, ResultType.TIME);
+        Disciplin displin7 = new Disciplin("Højdespring", DisciplineType.HØJDESPIRING, ResultType.DISTANCE);
+        Disciplin displin8 = new Disciplin("Stangspring", DisciplineType.STANGSPRING, ResultType.DISTANCE);
+        Disciplin displin9 = new Disciplin("Længdespring", DisciplineType.LÆNGDESPRING, ResultType.DISTANCE);
+        Disciplin displin10 = new Disciplin("Kuglestød", DisciplineType.KUGLESTØD, ResultType.DISTANCE);
+        Disciplin displin11 = new Disciplin("Diskoskast", DisciplineType.DISKOSKAST, ResultType.DISTANCE);
+        Disciplin displin12 = new Disciplin("Spydkast", DisciplineType.SPYDKAST, ResultType.DISTANCE);
 
-        disciplinRepository.saveAll(Arrays.asList(disciplin1, disciplin2, disciplin3));
+
+        disciplinRepository.saveAll(Arrays.asList(disciplin1, disciplin2, disciplin3, displin4, displin5, displin6, displin7, displin8, displin9, displin10, displin11, displin12));
 
         // Link participants and disciplines
         participant1.setDisciplins(new HashSet<>(Arrays.asList(disciplin1)));
         participant2.setDisciplins(new HashSet<>(Arrays.asList(disciplin2)));
         participant3.setDisciplins(new HashSet<>(Arrays.asList(disciplin3)));
+
 
         participantRepository.saveAll(Arrays.asList(participant1, participant2, participant3));
 
